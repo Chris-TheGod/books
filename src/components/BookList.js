a146ee70156a5678093726067cb5a97b79b96cab
Kristjan Lääne <xonproject@gmail.com>
2023-05-29 18:08:46 +0300

    displaying the list

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { BookShow } from "./BookShow";
 
-export const BookList = () => {
-  return <div>BookList</div>;
+export const BookList = ({ books }) => {
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 };
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const BookShow = () => {
-  return <div>BookShow</div>;
+export const BookShow = ({ book }) => {
+  return <div className="book-show">{book.title}</div>;
 };
